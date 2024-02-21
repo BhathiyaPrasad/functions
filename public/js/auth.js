6a12f876d5b15c025ba073e6632501d6642b0c49
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-02-21 10:28:24 +0530

    auth listener

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,3 @@
-const { user } = require("firebase-functions/v1/auth");
-
 
 const authSwitchLinks = document.querySelectorAll('.switch');
 const authModals = document.querySelectorAll('.auth .modal');
@@ -15,7 +13,7 @@ authSwitchLinks.forEach(link => {
     authModals.forEach(modal => modal.classList.toggle('active'));
   });  
 });
- // for the register from
+ 
 
  registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -23,14 +21,63 @@ authSwitchLinks.forEach(link => {
 const email = registerForm.email.value;
 
 const password = registerForm.password.value;
-console.log(email, password);
-firebaseConfig.auth().createUserWithEmailAndPassowrd(email, password)
+firebase.auth().createUserWithEmailAndPassword(email, password)
 .then(user => {
     console.log( 'registered', user);
-    register.reset();   
+    registerForm.reset();   
 })
 .catch((error) =>{
-    
-})
-})
+   registerForm.querySelector('.error').textContent = error.message;
+});
+});
 
+// login form
+
+authSwitchLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    authModals.forEach(modal => modal.classList.toggle('active'));
+  });  
+});
+ 
+
+ loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+const email = loginForm.email.value;
+
+const password = loginForm.password.value;
+firebase.auth().signInWithEmailAndPassword(email, password)
+.then(user => {
+    console.log( 'Logged in', user);
+    loginForm.reset();   
+})
+.catch((error) =>{
+   loginForm.querySelector('.error').textContent = error.message;
+});
+});
+
+// auth listener
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // Check if authWrapper is defined and not null
+    if (authWrapper && authWrapper.length > 0) {
+      authWrapper[0].classList.remove('open');
+    }
+
+    // Check if authModals is defined and not null
+    if (authModals && authModals.length > 0) {
+      authModals.forEach(modal => modal.classList.remove('active'));
+    }
+  } else {
+    // Check if authWrapper is defined and not null
+    if (authWrapper && authWrapper.length > 0) {
+      authWrapper[0].classList.add('open');
+    }
+
+    // Check if authModals is defined and not null
+    if (authModals && authModals.length > 0) {
+      authModals[0].classList.add('active');
+    }
+  }
+});
