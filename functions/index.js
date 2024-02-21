dbd5ee29b7d10c1a1b6e98ba5ec3bd795840886b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-02-21 12:53:49 +0530

    update

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 
 // auth trigger (new user sign up)
 exports.newUserSignup= functions.auth.user().onCreate((user) => {
-  console.log("user created", user.email, user.uid);
+  admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upvotedOn: []
+  });
+
 });
 
 // user deleted
