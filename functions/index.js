dea95bb7f8d4964017bc589b0ca0ee5462ab1329
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-02-21 11:51:38 +0530

    deleted tested functions and created functions for monitor new user sigtn up and user deleted

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,12 @@
 const functions = require("firebase-functions");
 
 
-// http one
-
-exports.randomNumber = functions.https.onRequest((request, response) => {
-  const number = Math.round(Math.random() * 100);
-  console.log(number);
-  response.send(number.toString());
+// auth trigger (new user sign up)
+exports.newUserSignup= functions.auth.user().onCreate((user) => {
+  console.log("user created", user.email, user.uid);
 });
 
-
-exports.order = functions.https.onRequest((request, response) => {
-  const numbers = "Bhathiya";
-  const numberss = "Prasad";
-  response.send(numbers + numberss);
+// user deleted
+exports.newUserdeleted= functions.auth.user().onDelete((user) => {
+  console.log("user deleteed", user.email, user.uid);
 });
-
-
-exports.customer = functions.https.onRequest((request, response) => {
-  const number1 = 12;
-  const number2 = 15;
-  const result = number1 + number2;
-  response.send(result.toString());
-});
-
