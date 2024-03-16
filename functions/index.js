f7a5e210ff781e101954766006483a8f99f45035
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-03-16 10:20:30 +0530

    resolving sysntax issues

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,17 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 
 // auth trigger (new user sign up)
-exports.newUserSignup= functions.auth.user().onCreate((user) => {
-  admin.firestore().collection('users').doc(user.uid).set({
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+  admin.firestore().collection("users").doc(user.uid).set({
     email: user.email,
-    upvotedOn: []
+    upvotedOn: [],
   });
-
 });
 
 // user deleted
-exports.newUserdeleted= functions.auth.user().onDelete((user) => {
+exports.newUserdeleted = functions.auth.user().onDelete((user) => {
   console.log("user deleteed", user.email, user.uid);
 });
